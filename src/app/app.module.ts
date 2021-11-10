fe45183339768d7c6127e48ac7df51efe6e6ca6c
snehabayani15@gmail.com <snehabayani15@gmail.com>
2021-11-10 20:07:06 +0530

    new updated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleChartsModule } from 'angular-google-charts';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    GoogleChartsModule,
+    AppRoutingModule,
+   
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
